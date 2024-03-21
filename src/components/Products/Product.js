b28289eb20b47f95b969eeecee0b6b780d25fb25
developerrayhan <developerrayhan15@gmail.com>
2024-03-21 13:51:25 +0600

    update product card

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -4,6 +4,7 @@ import React from "react";
 import { addToCart } from "@/redux/slices/cartSlice";
 import { useDispatch } from "react-redux";
 import { Zoom, toast } from "react-toastify";
+import Link from "next/link";
 
 const Product = ({ product }) => {
     const dispatch = useDispatch();
@@ -25,35 +26,45 @@ const Product = ({ product }) => {
     }
 
     return (
-        <article className="relative flex flex-col overflow-hidden rounded-lg border">
-            <div className="aspect-square overflow-hidden">
-                <Image
-                    className="object-cover transition-all duration-300 group-hover:scale-125"
-                    src={thumbnail}
-                    alt={title}
-                    width={300}
-                    height={300}
-                />
+        <div class="relative flex w-full flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+            <div class="relative flex h-60 overflow-hidden">
+                <Image class="object-cover" src={thumbnail} alt={title} width={300} height={300} />
+                <span class="absolute top-0 left-0 m-2 rounded-full bg-primary px-2 text-center text-sm font-medium text-white">
+                    sale
+                </span>
             </div>
-            <div className="absolute top-0 m-2 rounded-full bg-white">
-                <p className="rounded-full bg-emerald-500 p-1 text-[8px] font-bold uppercase tracking-wide text-white sm:py-1 sm:px-3">
-                    Sale
-                </p>
-            </div>
-            <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
-                <h2 className="text-lg font-bold text-gray-800 line-clamp-1">{title}</h2>
-                <p className="mb-2 text-sm text-gray-400">{category}</p>
-                <p className="mr-3 text-sm font-semibold mb-2">${price}</p>
-            </div>
-            <button
-                className="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600"
-                onClick={() => hadleAddToCart()}
-            >
-                <div className="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-emerald-600 group-hover:text-white">
-                    Add to cart
+            <div class="mt-4 px-5 pb-5">
+                <Link href="#">
+                    <h5 class="text-xl tracking-tight text-slate-900 line-clamp-1">{title}</h5>
+                </Link>
+                <div class="mt-2 mb-5 flex flex-col gap-2">
+                    <p class="text-sm font-medium text-gray-500">{category}</p>
+                    <p>
+                        <span class="text-xl font-bold text-slate-900">${price}</span>
+                    </p>
                 </div>
-            </button>
-        </article>
+                <button
+                    class="flex items-center justify-center rounded-md bg-primary px-5 py-2.5 text-center text-sm font-medium text-white w-full"
+                    onClick={() => hadleAddToCart()}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="mr-2 h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                        />
+                    </svg>
+                    Add to cart
+                </button>
+            </div>
+        </div>
     );
 };
 
