37a2d62779866e797ceff95e181dadbc05100e5c
developerrayhan <developerrayhan15@gmail.com>
2024-03-21 13:54:26 +0600

    update product card

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -4,7 +4,7 @@ import Product from "./Product";
 const Products = ({ products }) => {
     const Allproduct = products.products;
     return (
-        <div className="mt-10 grid grid-cols-2 gap-6 sm:grid-cols-4 sm:gap-4 lg:mt-16">
+        <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 gap-6 lg:grid-cols-4 sm:gap-4 lg:mt-16">
             {Allproduct.map((product) => (
                 <Product key={product.id} product={product} />
             ))}
